1b3a4be45fd50179e362e65bbd64f046701ab9f4
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-16 15:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //your JS code here. If required.
-function functionName() {
-  alert("functionName");
+
+function functionName(){
+	alert("functionName");
 }
 
-function AccioJob() {
-  alert("AccioJob");
+functionName();
+
+function AccioJob(){
+	alert("AccioJob");
 }
 
+AccioJob();
